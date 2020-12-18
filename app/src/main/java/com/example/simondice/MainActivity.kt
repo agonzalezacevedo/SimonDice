206ead83e8af2db0a7e2d3a22318a37c20fcbc2c
agonzalezacevedo <agonzalezacevedo@danielcastelao.org>
2020-12-18 18:51:07 +0100

    Final

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -10,10 +10,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var random: Int = (0..3).random()
+        val fourColors = arrayOf("Green", "Yellow", "Blue", "Red")
+        val allColors: ArrayList<String> = arrayListOf(fourColors[random])
+        val activitiesArray = arrayOf(Green::class.java, Yellow::class.java, Blue::class.java, Red::class.java)
+
+
+        for(i in 0..3){
+            random = (0..3).random()
+            allColors.add(fourColors[random])
+        }
         val botonEmpezar = findViewById<Button>(R.id.botonEmpezar)
         botonEmpezar.setOnClickListener {
-            val intent = Intent(this,Game::class.java)
+            val intent = Intent(this@MainActivity, activitiesArray[random])
+            intent.putStringArrayListExtra("colors", allColors)
+            intent.putExtra("count", 0)
+            intent.putExtra("score", 0)
             startActivity(intent)
         }
+
+
     }
 }
